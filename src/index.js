bbce4e172e6a8c6ce16082b8772b14c0173b6b96
Pete Fitton <petefitton@gmail.com>
2025-06-23 17:44:57 -0600

    Complete first half of code along

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import db from "../_db.js";
+// types
+import { typeDefs } from "../schema.js";
+
+const resolvers = {
+  Query: {
+    games() {
+      return db.games
+    },
+    authors() {
+      return db.authors
+    },
+    reviews() {
+      return db.reviews
+    },
+  }
+}
+
+// server setup
+const server = new ApolloServer({
+  // typedefs -- definitions of types of data -> author, game, etc
+  typeDefs,
+
+  // resolvers
+  resolvers
+})
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 }
+})
+
+
+console.log('Server ready at port ', 4000);
