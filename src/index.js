647b20ad90ec0865e4863bd891cc59ba2c797ba6
Pete Fitton <petefitton@gmail.com>
2025-06-23 18:52:35 -0600

    Finish code along

--- src/index.js
+++ src/index.js
@@ -9,12 +9,63 @@ const resolvers = {
     games() {
       return db.games
     },
+    game(_, args) {
+      return db.games.find((author) => author.id === args.id)
+    },
     authors() {
       return db.authors
+    },
+    author(_, args) {
+      return db.authors.find((game) => game.id === args.id)
     },
     reviews() {
       return db.reviews
     },
+    review(_, args) {
+      return db.reviews.find((review) => review.id === args.id)
+    }
+  },
+  Game: {
+    reviews(parent) {
+      return db.reviews.filter((review) => review.game_id === parent.id)
+    }
+  },
+  Author: {
+    reviews(parent) {
+      return db.reviews.filter((review) => review.author_id === parent.id)
+    }
+  },
+  Review: {
+    author(parent) {
+      return db.authors.find((author) => author.id === parent.author_id)
+    },
+    game(parent) {
+      return db.games.find((game) => game.id === parent.game_id)
+    }
+  },
+  Mutation: {
+    deleteGame(_, args) {
+      db.games = db.games.filter((game) => game.id !== args.id)
+      return db.games
+    },
+    addGame(_, args) {
+      let game = {
+        ...args.game,
+        id: Math.floor(Math.random() * 10000).toString()
+      }
+      db.games.push(game)
+      return game
+    },
+    updateGame(_, args) {
+      db.games = db.games.map((game) => {
+        if (game.id === args.id) {
+          return {...game, ...args.edits}
+        }
+
+        return game
+      })
+      return db.games.find(game => game.id === args.id)
+    }
   }
 }
 
